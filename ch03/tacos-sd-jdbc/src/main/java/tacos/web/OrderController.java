711ba84c2ce40517aa4d9052ed180343d071f504
fangkun119 <fangkun119@hotmail.com>
2021-03-01 19:58:23 +0800

    ch03 spring data

--- ch03/tacos-sd-jdbc/src/main/java/tacos/web/OrderController.java
+++ ch03/tacos-sd-jdbc/src/main/java/tacos/web/OrderController.java
@@ -1,6 +1,7 @@
 package tacos.web;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -20,6 +21,7 @@ public class OrderController {
   
   private OrderRepository orderRepo;
 
+  @Autowired
   public OrderController(OrderRepository orderRepo) {
     this.orderRepo = orderRepo;
   }
